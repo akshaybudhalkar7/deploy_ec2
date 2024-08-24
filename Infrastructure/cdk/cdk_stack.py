a12c4f386a7d3dacc6f5e012d84d5d7f19b7e977
TruptiAB <165988003+TruptiAB@users.noreply.github.com>
2024-08-24 23:47:57 +0530

    added ec2

--- Infrastructure/cdk/cdk_stack.py
+++ Infrastructure/cdk/cdk_stack.py
@@ -11,32 +11,33 @@ class DemoStack(Stack):
         super().__init__(scope,id,**kwargs)
 
         # Create a VPC
-         vpc = ec2.Vpc(self, "MyVpc",
+        vpc = aws_ec2.Vpc(self, "MyVpc",
          max_azs=3,  # Default is all AZs in the region
                               cidr="10.0.0.0/16",
                               subnet_configuration=[
-                                  ec2.SubnetConfiguration(
+                                  aws_ec2.SubnetConfiguration(
                                       cidr_mask=24,
                                       name="PublicSubnet",
-                                      subnet_type=ec2.SubnetType.PUBLIC,
+                                      subnet_type=aws_ec2.SubnetType.PUBLIC,
                                   ),
-                                  ec2.SubnetConfiguration(
+                                  aws_ec2.SubnetConfiguration(
                                       cidr_mask=24,
                                       name="PrivateSubnet",
-                                      subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                                      subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
                                   ),
-                                  ec2.SubnetConfiguration(
+                                  aws_ec2.SubnetConfiguration(
                                       cidr_mask=28,
                                       name="IsolatedSubnet",
-                                      subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                                      subnet_type=aws_ec2.SubnetType.PRIVATE_ISOLATED,
                                   )
                               ]
+                              )
 
 
         # Create an EC2 instance
-        instance = ec2.Instance(self, "MyInstance",
-        instance_type=ec2.InstanceType("t2.micro"),
-        machine_image=ec2.MachineImage.latest_amazon_linux(),
+        instance = aws_ec2.Instance(self, "MyInstance",
+        instance_type=aws_ec2.InstanceType("t2.micro"),
+        machine_image=aws_ec2.MachineImage.latest_amazon_linux(),
         vpc=vpc,
         key_name="ec2_keypair"  # Replace with your key pair name
         )
