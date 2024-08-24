05821780bcf6f4848855e984bf31ecc6b2bd0520
TruptiAB <165988003+TruptiAB@users.noreply.github.com>
2024-08-24 23:57:05 +0530

    added ec2

--- Infrastructure/cdk/cdk_stack.py
+++ Infrastructure/cdk/cdk_stack.py
@@ -10,35 +10,35 @@ class DemoStack(Stack):
     def __init__(self, scope: Construct, id:str, environment:None, **kwargs) -> None:
         super().__init__(scope,id,**kwargs)
 
-        # Create a VPC
-        vpc = aws_ec2.Vpc(self, "MyVpc",
-         max_azs=3,  # Default is all AZs in the region
-                              cidr="10.0.0.0/16",
-                              subnet_configuration=[
-                                  aws_ec2.SubnetConfiguration(
-                                      cidr_mask=24,
-                                      name="PublicSubnet",
-                                      subnet_type=aws_ec2.SubnetType.PUBLIC,
-                                  ),
-                                  aws_ec2.SubnetConfiguration(
-                                      cidr_mask=24,
-                                      name="PrivateSubnet",
-                                      subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
-                                  ),
-                                  aws_ec2.SubnetConfiguration(
-                                      cidr_mask=28,
-                                      name="IsolatedSubnet",
-                                      subnet_type=aws_ec2.SubnetType.PRIVATE_ISOLATED,
-                                  )
-                              ]
-                              )
-
-
-        # Create an EC2 instance
-        instance = aws_ec2.Instance(self, "MyInstance",
-        instance_type=aws_ec2.InstanceType("t2.micro"),
-        machine_image=aws_ec2.MachineImage.latest_amazon_linux(),
-        vpc=vpc,
-        key_name="ec2_keypair"  # Replace with your key pair name
-        )
-
+#         # Create a VPC
+#         vpc = aws_ec2.Vpc(self, "MyVpc",
+#          max_azs=3,  # Default is all AZs in the region
+#                               cidr="10.0.0.0/16",
+#                               subnet_configuration=[
+#                                   aws_ec2.SubnetConfiguration(
+#                                       cidr_mask=24,
+#                                       name="PublicSubnet",
+#                                       subnet_type=aws_ec2.SubnetType.PUBLIC,
+#                                   ),
+#                                   aws_ec2.SubnetConfiguration(
+#                                       cidr_mask=24,
+#                                       name="PrivateSubnet",
+#                                       subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
+#                                   ),
+#                                   aws_ec2.SubnetConfiguration(
+#                                       cidr_mask=28,
+#                                       name="IsolatedSubnet",
+#                                       subnet_type=aws_ec2.SubnetType.PRIVATE_ISOLATED,
+#                                   )
+#                               ]
+#                               )
+#
+#
+#         # Create an EC2 instance
+#         instance = aws_ec2.Instance(self, "MyInstance",
+#         instance_type=aws_ec2.InstanceType("t2.micro"),
+#         machine_image=aws_ec2.MachineImage.latest_amazon_linux(),
+#         vpc=vpc,
+#         key_name="ec2_keypair"  # Replace with your key pair name
+#         )
+#
